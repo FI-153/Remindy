329d468e8e7aafb49670120c19b0fa0dade22c1e
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-06-11 15:20:13 +0200

    Initial Commit

---
+++ Remindy/RemindyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindyApp.swift
+//  Remindy
+//
+//  Created by Federico Imberti on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
